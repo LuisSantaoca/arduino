bcc56981088d7177ff77fada95b9f82d5a23eb9d
Luis Ocaranza <luis.ocaranza@intelagro.mx>
2024-09-10 09:11:37 -0600

    240910

---
+++ esp32/esp32.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		esp32.ino
+ Created:	04/09/2024 11:48:11
+ Author:	Luis Ocaranza
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
